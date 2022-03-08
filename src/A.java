1caf434b26c6069d6d5ce3befe6efd9ee2b2aa36
Avinash Verma <avinashverma657@gmail.com>
2022-03-08 17:32:00 +0530

    first commit..

---
+++ src/A.java
@@ -0,0 +1,12 @@
+
+public class A {
+	public static void main(String[] args) {
+		String str = "hungry";
+		if(str=="hungry") {
+			System.out.println("Khana Kha lo");
+		}
+		else {
+			System.out.println("Nikal lo");
+		}
+	}
+}	
